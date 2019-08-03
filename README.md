6067f39921745c783037c2f3821a0fdea31ca07c
kamiokan <spherenov@gmail.com>
2019-08-03 20:45:53 +0900

    First commit!

---
+++ README.md
@@ -0,0 +1,12 @@
+# go-wiki-ayame
+====
+
+## Description
+
+https://golang.org/doc/articles/wiki/
+
+Go公式チュートリアルのwiki（WEBアプリを作る）の部分を実装
+
+## Author
+
+kamioka nobuhiro
---
+++ net-http.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// https://golang.org/doc/articles/wiki/
+// Introducing the net/http package (an interlude)
